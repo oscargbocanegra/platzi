2b85e2e9a96b403797e6eeadf82e60850ca7781a
oscar giovanni <oscargiovanni@gmail.com>
2022-02-25 18:17:25 -0500

    actualizacion version palindromo con try except

--- python-programming/exercises/palindromo.py
+++ python-programming/exercises/palindromo.py
@@ -1,20 +1,29 @@
 def palindromo(palabra):
-    palabra = palabra.replace(' ', '')
-    palabra = palabra.lower()
-    palabra_invertida = palabra[::-1]
-
-    if palabra == palabra_invertida:
+    try:
+        if len(palabra) == 0:
+            raise ValueError("No se puede ingresar una cadena vacia")
+        else:
+            palabra = palabra.replace(' ', '')
+            palabra = palabra.lower()
+            palabra_invertida = palabra[::-1]
+            palabra == palabra_invertida
         return True
-    else:
+
+    except ValueError as ve:
+        print(ve)
         return False
 
 def run():
     palabra = input('Escribe una palabra: ')
-    es_palindromo = palindromo(palabra)
-    if es_palindromo == True:
-        print('Es palindromo:')
-    else:
-        print('No es palindromo:')
+    #palabra = 1
+    try:
+        es_palindromo = palindromo(palabra)
+        if es_palindromo == True:
+            print('Es palindromo:')
+        else:
+            print('No es palindromo:')
+    except TypeError:
+        print('Solo se pueden ingresar Strings')
 
 if __name__ == '__main__':
     run()
--- python-programming/proy_ent_virtual/filtrando_datos.py
+++ python-programming/proy_ent_virtual/filtrando_datos.py
@@ -72,19 +72,25 @@ DATA = [
 ]
 
 def run():
-    # Creando filtros para todos los programadores de python.
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    # Creando filtros para todos los trabajadores que pertenecen a platzi
-    all_python_devs = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    # Creando filtros con filter para todos los que tienen edad mayor a como
+    # Comprehensions solutions
+    # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # adults =  [worker["name"] for worker in DATA if worker["age"] > 18]
+    # old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+
+    # Challenge completed
+    all_python_devs = list(filter(lambda worker: worker["language"] == "python", DATA))
+    all_python_devs = list(map(lambda worker: worker["name"], all_python_devs))
+    
+    all_Platzi_workers = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+    all_Platzi_workers = list(map(lambda worker: worker["name"], all_Platzi_workers))
+
     adults = list(filter(lambda worker: worker["age"] > 18, DATA))
-    # Convinando el diccionario con map para obteenr solo los nombres.
     adults = list(map(lambda worker: worker["name"], adults))
-    # Agregando una nueva llave.
-    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
 
+    old_people = [worker | {"old": worker["age"] > 70} for worker in DATA]
 
-    for worker in old_people:
+    for worker in all_python_devs:
         print(worker)
 
 if __name__ == '__main__':
